c9e87e64f10ea606480692cae1839e54b6da7d74
sophieqgu <sophieqgu@gmail.com>
2025-07-30 17:35:39 -0400

    Style RewardsHero

--- app/components/Nav/Main/MainNavigator.js
+++ app/components/Nav/Main/MainNavigator.js
@@ -530,7 +530,6 @@ const HomeTabs = () => {
         );
       },
       rootScreenName: Routes.REWARDS_VIEW,
-      unmountOnBlur: true,
     },
     settings: {
       tabBarIconKey: TabBarIconKey.Setting,
